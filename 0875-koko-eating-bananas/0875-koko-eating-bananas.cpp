af1c214589318fbfe3150d8a8269a6f1baeb9393
dev20206 <147929385+dev20206@users.noreply.github.com>
2024-02-08 23:00:26 +0530

    Time: 31 ms (78.53%), Space: 21.3 MB (36.16%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,42 +1,30 @@
-#include <vector>
-#include <cmath>
-#include <algorithm>
-#include <climits>
-
-using namespace std;
-
 class Solution {
 public:
-    int findmax(vector<int>& v) {
-        int maxi = INT_MIN;
-        for(int i = 0; i < v.size(); i++) {
-            maxi = max(maxi, v[i]);
-        }
-        return maxi;
-    }
-    
-    int64_t calculatetotalhours(vector<int>& v, int hourly) {
-        int64_t totalh = 0;
-        int n = v.size();
-        for(int i = 0; i < n; i++) {
-            totalh += ceil(static_cast<double>(v[i]) / hourly);
-        }
-        return totalh;
-    }
-    
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low = 1;
-        int high = findmax(piles);
-        while(low <= high) {
-            int mid = low + (high - low) / 2;
-            int64_t totalh = calculatetotalhours(piles, mid);
-            if(totalh <= h) {
-                high = mid - 1;
+        int maxi=INT_MIN;
+        for(auto i:piles){
+            maxi=max(maxi,i);
+        }
+        long long int low=1;
+        long long int high=maxi;
+        long long int res=high;
+        while(low<=high){
+            long long int mid=(high+low)/2;
+            long long int count=0;
+            for(auto i:piles){
+                count+=(i/mid);
+                if(i%mid!=0){
+                    count++;
+                }
             }
-            else {
-                low = mid + 1;
+            if(count<=h){
+                res=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
-        return low;
+        return res;
     }
 };
