063a0ec5205b1f6fe5a45ccc37d6f0263f89df0e
dev20206 <147929385+dev20206@users.noreply.github.com>
2024-07-22 18:26:20 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.53%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,30 +1,25 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)
-            return 0;
-        if(nums[0]>nums[1])
-            return 0;
-        if(nums[n-1]>nums[n-2])
-            return n-1;
+        int n = nums.size();
         
-        int low=1 , high=n-2;
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            if(nums[mid]>=nums[mid-1] && nums[mid]>=nums[mid+1])
-            {
-                return mid;
-            }
-            else if(nums[mid]>nums[mid-1])
-            {
-                low=mid+1;
-            }
-            else
-                high=mid-1;
+        // Handle edge cases
+        if (n == 1) return 0;
+        if (n == 2) return nums[0] > nums[1] ? 0 : 1;
+        
+        // Check for peak element
+        for (int i = 0; i < n; ++i) {
+            // If the current element is the first element
+            if (i == 0 && nums[i] > nums[i + 1]) return i;
+            // If the current element is the last element
+            if (i == n - 1 && nums[i] > nums[i - 1]) return i;
+            // If the current element is greater than its neighbors
+            if (i > 0 && i < n - 1 && nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
         }
-        return -1;
         
+        return -1;  // This line will never be reached because there is always at least one peak element
     }
 };
