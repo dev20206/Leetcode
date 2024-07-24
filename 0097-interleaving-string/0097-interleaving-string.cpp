40b7e1d6ce7f6d1a3b2eac1ffbd6f5562d128138
dev20206 <147929385+dev20206@users.noreply.github.com>
2024-07-25 00:29:03 +0530

    Time: 6 ms (51.15%), Space: 8.6 MB (54.66%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    bool f(string &s1, string &s2, string &s3, int i, int j, int o) {
-        if (o == 0) {
-            return i == 0 && j == 0;
-        }
-
-        if (i > 0 && s1[i - 1] == s3[o - 1] && f(s1, s2, s3, i - 1, j, o - 1)) {
-            return true;
-        }
-        if (j > 0 && s2[j - 1] == s3[o - 1] && f(s1, s2, s3, i, j - 1, o - 1)) {
-            return true;
-        }
-
-        return false;
-    }
-
     bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.size();
-        int m = s2.size();
-        int o = s3.size();
-        if (n + m != o) {
-            return false;
+        if (s1.size() + s2.size() != s3.size()) return false;
+        
+        vector<vector<bool>> dp(s1.size() + 1, vector<bool>(s2.size() + 1, false));
+        dp[s1.size()][s2.size()] = true;
+        
+        for (int i = s1.size(); i >= 0; --i) {
+            for (int j = s2.size(); j >= 0; --j) {
+                if (i < s1.size() && s1[i] == s3[i + j] && dp[i + 1][j]) {
+                    dp[i][j] = true;
+                }
+                if (j < s2.size() && s2[j] == s3[i + j] && dp[i][j + 1]) {
+                    dp[i][j] = true;
+                }
+            }
         }
-        return f(s1, s2, s3, n, m, o);
+        
+        return dp[0][0];
     }
 };
