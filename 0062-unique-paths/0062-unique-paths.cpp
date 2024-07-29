48aa78614cf890664d93c3a67a62d9b870247188
dev20206 <147929385+dev20206@users.noreply.github.com>
2024-07-29 10:54:47 +0530

    Time: 0 ms (100%), Space: 8 MB (7.22%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-
-    int f(int i , int j, vector<vector<int>>&dp)
-    {
-        int up,left;
-        if(i == 0 && j==0)
-        {
-            return 1;
-        }
-
-        if(i<0 || j<0)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        else{
-        up = f(i-1,j,dp);
-        left = f(i,j-1,dp);
-
-        return dp[i][j]=up+left;
-        }
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return f(m-1,n-1,dp);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        // Initialize the first row and first column
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+
+        // Fill the dp table
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
