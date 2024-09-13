e012f6962df381c125dd784564b3ceaf03f6d606
dev20206 <147929385+dev20206@users.noreply.github.com>
2024-09-13 10:59:10 +0530

    Time: 0 ms (100%), Space: 7.6 MB (86.39%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        // If the lengths are different, goal can't be a rotation of s
-        if (s.size() != goal.size()) {
+         if (s.size() != goal.size()) {
             return false;
         }
+        string final = s+s;
+        if(final.find(goal)!=string::npos)
+        {
+            return true;
 
-        // Concatenate s with itself to cover all possible rotations
-        string final = s + s;
-
-        // Check if goal is a substring of the concatenated string
-        return final.find(goal) != string::npos;
+        }
+        return false;
     }
 };
