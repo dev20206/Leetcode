94ae2d358872d597edf862611844ed87847c141a
dev20206 <147929385+dev20206@users.noreply.github.com>
2024-09-18 23:50:36 +0530

    Time: 95 ms (56.78%), Space: 57.7 MB (99.56%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,8 +1,34 @@
 class Solution {
 public:
+    int partition(vector<int>& arr, int left, int right) {
+        int pivot = arr[left];  // Use the leftmost element as the pivot
+        int l = left + 1;
+        int r = right;
+
+        while (l <= r) {
+            if (arr[l] < pivot && arr[r] > pivot) {
+                swap(arr[l], arr[r]);
+                l++;
+                r--;
+            }
+            if (arr[l] >= pivot) l++;
+            if (arr[r] <= pivot) r--;
+        }
+        swap(arr[left], arr[r]);
+        return r;
+    }
+
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()-k];
-        
+        int left = 0, right = nums.size() - 1;
+        while (true) {
+            int idx = partition(nums, left, right);
+            if (idx == k - 1) {
+                return nums[idx];
+            } else if (idx < k - 1) {
+                left = idx + 1;
+            } else {
+                right = idx - 1;
+            }
+        }
     }
 };
