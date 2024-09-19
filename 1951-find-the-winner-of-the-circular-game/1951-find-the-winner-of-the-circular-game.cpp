6a3d0f897ebb380f2487bdd4d64b8d965562e122
dev20206 <147929385+dev20206@users.noreply.github.com>
2024-09-20 00:20:15 +0530

    Time: 0 ms (100%), Space: 7.4 MB (94.66%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
 
-    void recursion(vector<int>&array , int k , int index)
-    {
-        int n = array.size();
-        if(n==1)
-        {
-            return;
+    int findTheWinner(int N, int k) {
+        int i = 1, ans = 0;
+
+        // Run a while loop till i <= N
+
+        while (i <= N) {
+
+        // Update the Value of ans and Increment i by 1
+        ans = (ans + k) % i;
+        i++;
         }
 
-        index = ((index+k) % n);
-        array.erase(array.begin()+index);
-        recursion(array, k , index);
-    }
-    int findTheWinner(int n, int k) {
-        vector<int>array;
-        for(int i = 1 ; i<=n ; i ++)
-        {
-            array.push_back(i);
-        }
-
-        recursion(array,k-1,0);
-
-        return array[0];
-        
+    // Return required answer
+    return ans + 1;
     }
 };
