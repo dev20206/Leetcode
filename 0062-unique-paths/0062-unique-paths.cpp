68899fb72a38c5b6dc3d56dd0fb074143a26629a
dev20206 <147929385+dev20206@users.noreply.github.com>
2024-09-20 11:28:58 +0530

    Time: 0 ms (100%), Space: 7.8 MB (63.84%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-    vector<vector<int>> dp(m, vector<int>(n, -1));
-       for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            // Base condition: If we are at the top-left cell (0, 0), there is one way.
-            if (i == 0 && j == 0) {
-                dp[i][j] = 1;
-                continue; // Skip the rest of the loop and continue with the next iteration.
-            }
-
-            // Initialize variables to store the number of ways from the cell above (up) and left (left).
-            int up = 0;
-            int left = 0;
-
-            // If we are not at the first row (i > 0), update 'up' with the value from the cell above.
-            if (i > 0)
-                up = dp[i - 1][j];
-
-            // If we are not at the first column (j > 0), update 'left' with the value from the cell to the left.
-            if (j > 0)
-                left = dp[i][j - 1];
-
-            // Calculate the number of ways to reach the current cell by adding 'up' and 'left'.
-            dp[i][j] = up + left;
+    int recursion(int i , int j, vector<vector<int>>&dp)
+    {
+        if(i==0 || j==0)
+        {
+            return 1;
         }
+        if(i<0 || j<0)
+        {
+            return 0;
+        }
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int up = recursion(i,j-1,dp);
+        int left = recursion(i-1,j,dp);
+
+        return dp[i][j]=up + left;
     }
 
-    // The result is stored in the bottom-right cell (m-1, n-1).
-    return dp[m - 1][n - 1];
-}
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m, vector<int>(n, -1));
+        return recursion(m-1,n-1,dp);
+        
+    }
 };
