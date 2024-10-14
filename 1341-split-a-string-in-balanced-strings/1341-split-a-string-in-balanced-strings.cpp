e3900b15cf99dd85c4b813d3fc79c66f32398aac
dev20206 <147929385+dev20206@users.noreply.github.com>
2024-10-14 23:55:27 +0530

    Time: 0 ms (100%), Space: 7.7 MB (58.54%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cpp
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int balancedStringSplit(string s) {
         int countr = 0;
-        int countl = 0;
         int ans = 0;
         for(int i = 0 ; i < s.size() ; i++)
         {
             if(s[i]=='R')
                 countr++;
             if(s[i]=='L')
-                countl++;
+                countr--;
             
-            if(countl == countr){
-                countl = countr = 0;
+            if(countr == 0){
                 ans++;
             }
         }
